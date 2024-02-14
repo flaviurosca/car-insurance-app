54b7897e5defc2a577f61e76c06141e316e5bef9
Flaviu Rosca <flaviuroscaivanciuc@gmail.com>
2024-02-14 17:22:55 +0200

    small changes on footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,13 +2,7 @@ import { Box, Flex, Text } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
-    <Box
-      as="footer"
-      borderTop="1px solid"
-      borderColor="gray.500"
-      bg="gray.800"
-      py={4}
-    >
+    <Box as="footer" bg="gray.700" py={4}>
       <Flex justifyContent="center">
         <Text fontSize="sm" color="gray.500">
           &copy; {new Date().getFullYear()} RoadMaster Insurance. All Rights
